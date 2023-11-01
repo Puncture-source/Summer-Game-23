4f90c31b1e39a501f984186b10754a0e1fa7e574
Pierce O'Connor <74575565+Puncture-source@users.noreply.github.com>
2023-11-01 11:31:04 +1100

    Changed Basic Types
    hp types to well point
    levelup to player class
    moved classes into player class

--- allCharacters.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "Jackal.h"
-#include "Zealot.h"
---
+++ pointWell.h
@@ -0,0 +1,78 @@
+#pragma once
+#include <cstdint>
+
+typedef std::uint16_t wellType;
+
+class pointWell {
+public:
+
+//sets max hp (like from level up)
+//return true if set correctly 
+    bool setMax(wellType newMax){
+        if (newMax <1){
+            return false;
+        }
+        max = newMax;
+        if (current > max){
+            current = max;
+        }
+    return true;
+}
+
+// allows access to private max hp info
+
+    wellType getMax(){
+        return max;
+    }
+    
+
+// see current health
+wellType getCurrent() {
+    return current;
+}
+
+//Ran when health loss occurs
+    void reduce(wellType damage){
+        if (damage > current){
+        current = 0;
+        return;
+        }
+
+    current -= damage;
+    }
+
+
+//runs when player gains health
+    void increase(wellType heal){
+        if (heal + current > max){
+            current = max;
+            return;
+        }
+        current += heal;
+        return;
+    }
+    
+
+
+//constructor for no input
+    pointWell(){
+        current =1;
+        max = 1;
+    }
+
+// constructor with input
+pointWell(wellType curr, wellType maxx){
+    if(curr>maxx){ //might need to fix this later, might cause issues
+        current = maxx;
+        max = maxx;
+        return;
+    }
+    current = curr;
+    max = maxx;
+}    
+
+private:
+    wellType shield;
+    wellType current;
+    wellType max;    
+};
